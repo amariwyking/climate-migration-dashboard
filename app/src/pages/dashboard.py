7444c718d14be76fe823606f28a42630cca9127e
Amari Garrett <amariwgarrett@gmail.com>
2025-03-12 18:20:59 -0400

    Create query to get county FIPS code after selection from the dropdown

--- app/src/pages/dashboard.py
+++ app/src/pages/dashboard.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-from src.utils import get_county_names
+from src.utils import get_all_county_names, get_county_fips_code
 
-st.selectbox(
+# TODO: Can we package the county name and FIPS code in the selectbox?
+county_name = st.selectbox(
     'Select a county',
-    get_county_names(),
+    get_all_county_names(),
     placeholder='Type to search...',
     index=None
 )
+
+# Get the County FIPS code, which will be used for all future queries
+if county_name:
+    county_fips = get_county_fips_code(county_name)
+else:
+    county_fips = None
--- app/src/utils.py
+++ app/src/utils.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-def get_county_names():
+def get_all_county_names():
+    # import table containing county names and FIPS
     counties = pd.read_csv('../data/county_names.csv')
 
     return counties.COUNTY_NAME
 
+def get_county_fips_code(county_name: str) -> str:
+    # import table containing county names and FIPS
+    counties = pd.read_csv('../data/county_names.csv')
+    
+    search_results = counties[counties.COUNTY_NAME == county_name]['COUNTY_FIPS']
+    
+    return search_results.iloc[0]
+    
+    
 def add_custom_css():
     st.markdown(
         """
